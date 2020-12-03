a40128f9448c486f6d73497193f5c69aaf218669
Snowdence <snowdence2911@gmail.com>
2020-12-03 23:29:27 +0700

    [init] Promise mongo

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -8,32 +8,32 @@
 const UserModel = require("../models/User");
 
 const getIndex = (req, res, next) => {
-  UserModel.find({}, (err, users) => {
-    if (err) {
-      return next(err);
-    }
-    console.log("Founded users ", users);
-  });
-
+  UserModel.find({})
+    .then((users) => {
+      console.log("Founded users: ", users);
+    })
+    .catch((err) => next(err));
   return res.status(200).json({
     message: "Users GET root",
   });
 };
 
 const newUser = (req, res, next) => {
-  console.log("request body ", req.body);
-
   const newUser = new UserModel(req.body);
   console.log(newUser);
-
-  newUser.save((err, user) => {
-    if (err) {
-      console.log("[ERROR] Existed user");
-    } else {
+  newUser
+    .save()
+    .then((user) => {
       console.log("Success to insert user");
       console.log(user);
-    }
-  });
+    })
+    .catch((err) => {
+      if (error.message.indexOf("11000") != -1) {
+        console.log("[ERROR] Existed user: " + err);
+      } else {
+        console.log("[ERROR] " + err);
+      }
+    });
 
   return res.status(200).json({
     message: "POST request to user root",
