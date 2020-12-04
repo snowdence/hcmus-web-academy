48fa23856a674b47414aae906b9c4921061a5d83
Snowdence <snowdence2911@gmail.com>
2020-12-04 14:59:52 +0700

    [login] Login form

--- src/routes/web.js
+++ src/routes/web.js
@@ -3,19 +3,32 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const authenticationMiddleware = require("../middleware/authentication");
+router.route("/").get((req, res) => res.render("pages/home"));
 router
   .route("/login")
   .get((req, res) => {
-    res.render("pages/login", { messages: req.flash("error") });
+    if (req.isAuthenticated()) {
+      res.redirect("/");
+    } else {
+      res.render("pages/login", {
+        layout: null,
+        message: req.flash("error"),
+      });
+    }
   })
   .post(
     passport.authenticate("local", {
       failureRedirect: "/login",
-      successRedirect: "/loginOK",
+      successRedirect: "/",
       failureFlash: true,
-      failureFlash: "Invalid username or passwerd.",
+      failureFlash: "Please check username or password!!!",
     })
   );
+
+router.route("/logout").get(function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
 
 router.route("/loginOK").get(authenticationMiddleware(), (req, res) => {
   res.send("Login thanh cong");
