3c5104b7c1064661b9d6ae814d3dbf84cf17f1c6
oOo-Solar-oOo <50949696+oOo-Solar-oOo@users.noreply.github.com>
2020-12-20 15:56:57 +0700

    [Home] View top courses (cont.)

--- src/controllers/web.js
+++ src/controllers/web.js
@@ -5,10 +5,12 @@ const getHomePage = async (req, res , next) => {
     const limTop5 = 5;
     const top10Courses = await courseModel.find().lean().limit(limTop10)
     const top5Courses = await  courseModel.find().lean().limit(limTop5).skip(limTop10)
+    const top10NewCourses = await  courseModel.find().lean().limit(limTop5).skip(limTop10+limTop5)
     res.render("pages/home", {
         title: "Home",
         top10Courses,
         top5Courses,
+        top10NewCourses,
     });
     
 }
