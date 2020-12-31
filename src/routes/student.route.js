4dd5d0b46da021a07afb3ecb5d7301d780b06144
oOo-Solar-oOo <50949696+oOo-Solar-oOo@users.noreply.github.com>
2021-01-01 00:38:51 +0700

    [Student] View student wishliist + courses

--- src/routes/student.route.js
+++ src/routes/student.route.js
@@ -6,4 +6,22 @@ router.route("/wishlist").get((req, res)=>{
         title: "Wishlist"
     })
 });
+
+router.route("/all-courses").get((req, res)=>{
+    res.render("pages/student/all-courses", {
+        title: "All Courses"
+    })
+});
+
+router.route("/review-courses").get((req, res)=>{
+    res.render("pages/student/review-courses", {
+        title: "Review Courses"
+    })
+});
+
+router.route("/archived").get((req, res)=>{
+    res.render("pages/student/archived", {
+        title: "Archived"
+    })
+});
 module.exports = router;
