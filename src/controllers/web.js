5eb84a066e83b3bd62eaae2a4f0a183f121d063d
Snowdence <snowdence2911@gmail.com>
2021-01-10 00:00:01 +0700

    [Fix] Home fix

--- src/controllers/web.js
+++ src/controllers/web.js
@@ -1,31 +1,33 @@
 const courseModel = require("../models/Course");
 
-const getHomePage = async (req, res , next) => {
-    const limTop10 = 10;
-    const limTop5 = 5;
-    const top10Courses = await courseModel.find().lean().limit(limTop10)
-    const top5Courses = await  courseModel.find().lean().limit(limTop5).skip(limTop10)
-    const top10NewCourses = await  courseModel.find().lean().limit(limTop5).skip(limTop10+limTop5)
-    res.render("pages/home", {
-        title: "Home",
-        top10Courses,
-        top5Courses,
-        top10NewCourses,
-    });
-    
-}
+const getHomePage = async (req, res, next) => {
+  const limTop10 = 10;
+  const limTop5 = 5;
+  const top10Courses = await courseModel.find().lean().limit(limTop10);
+  const top5Courses = await courseModel
+    .find()
+    .lean()
+    .limit(limTop5)
+    .skip(limTop10);
+  const top10NewCourses = await courseModel.find().lean().limit(limTop10);
+  res.render("pages/home", {
+    title: "Home",
+    top10Courses,
+    top5Courses,
+    top10NewCourses,
+  });
+};
 
-const courseDetail = async (req, res , next) => {
-    const query = courseModel.where({_id: req.params.id})
-    const course = await query.findOne().lean()
-    res.render("pages/courses/details", {
-        course,
-        title: 'Detail',
-    })
-    
-}
+const courseDetail = async (req, res, next) => {
+  const query = courseModel.where({ _id: req.params.id });
+  const course = await query.findOne().lean();
+  res.render("pages/courses/details", {
+    course,
+    title: "Detail",
+  });
+};
 
 module.exports = {
-    getHomePage,
-    courseDetail,
-}
+  getHomePage,
+  courseDetail,
+};
