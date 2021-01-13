75c3bfca460ab4bf6481fa3caed074344b57bc32
Anh Thu <18127226@student.hcmus.edu.vn>
2021-01-13 21:47:42 +0700

    [Admin] Student Mangement, Teacher Management
    Edit user

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,7 +1,12 @@
 const UserModel = require("../models/User");
 const CategoryModel = require("../models/Category");
+const bcrypt = require('bcrypt');
+
+const multer = require('multer');
+
+
 const getDashboard = (req, res, next) => {
-  res.render("pages/admin/dashboard", { layout: "main-admin" });
+  res.render("pages/admin/dashboard");
 };
 
 const getAllUser = async (req, res, next) => {
@@ -49,8 +54,109 @@ const getAllCategory = async (req, res, next) => {
       });
     });
 };
+const teacherManagement = async (req, res, next) => {
+  let teacher = await UserModel.find({role: 1}).lean()
+  res.render('pages/admin/teacher', {teacher})
+}
+
+const studentManagement = async (req, res, next) => {
+  let student = await UserModel.find({role: 2}).lean()
+  res.render('pages/admin/student', {student})
+}
+
+//[GET]
+const editUser = async (req, res, next) => {
+  let user = await UserModel.findOne({_id: req.params.id}).lean()
+  console.log('user: ', user)
+  res.render('pages/admin/editUserInfo',{user})
+}
+
+//[POST] /admin/user edit
+const editedUser = async (req, res, next) => {
+    var storage = multer.diskStorage({
+
+      destination: function (req, file, cb) {
+        cb(null, './src/public/img')
+      },
+      filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now()+'.png')
+      }
+    })
+  var upload = multer({ storage: storage })
+  upload.single('avatar')(req,res, async function(err){
+    console.log('debug: ', req.file)
+      var newData = JSON.parse(JSON.stringify(req.body))
+      if(typeof req.file != 'undefined')
+      {
+          newData.avatar = '/img/' + req.file.filename
+          console.log("change")
+      }
+      else
+          delete newData.avatar
+      await UserModel.updateOne({_id: req.params.id}, newData).lean()
+      res.send(req.body)
+ })
+}
+const postUserChangePassword = async (req, res, next) => {
+  const userPW = req.user.password
+  const pwChange = req.body.curPassword
+  const isMatch = await bcrypt.compare(pwChange, userPW);
+  console.log(isMatch)
+  if (
+    req.user.password === req.body.curPassword &&
+    req.body.password === req.body.verPassword
+  ) {
+    UserModel.updateOne({ username: req.user.username }, req.body)
+      .then()
+      .catch((err) => next(err));
+    res.render("pages/user/change-password", {
+      userAvatar: req.user.avatar,
+      userFullname: req.user.fullname,
+      userPhone: req.user.phone,
+      userEmail: req.user.email,
+      isUpdateSuccessfully: true,
+    });
+  } else {
+    if (req.user.password !== req.body.curPassword) {
+      res.render("pages/user/change-password", {
+        userAvatar: req.user.avatar,
+        userFullname: req.user.fullname,
+        userPhone: req.user.phone,
+        userEmail: req.user.email,
+        isFail: true,
+        message: "Wrong password! Please enter again!",
+      });
+    } else {
+      res.render("pages/user/change-password", {
+        userAvatar: req.user.avatar,
+        userFullname: req.user.fullname,
+        userPhone: req.user.phone,
+        userEmail: req.user.email,
+        isFail: true,
+        message: "Verified password doesn't match! Check it again",
+      });
+    }
+  }
+};
+const getUserChangePassword = (req, res, next) => {
+  let user = UserModel.findOne({_id: req.params.id}).lean()
+  
+  res.render("pages/user/change-password", {
+    userAvatar: user.avatar,
+    userFullname: user.fullname,
+    userPhone: user.phone,
+    userEmail: user.email,
+    title: "Change Password",
+  });
+}
 module.exports = {
   getDashboard,
   getAllUser,
   getAllCategory,
+  teacherManagement,
+  studentManagement,
+  editUser,
+  editedUser,
+  postUserChangePassword,
+  getUserChangePassword,
 };
--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -4,4 +4,11 @@ const AdminController = require("../controllers/admin");
 
 router.route("/").get(AdminController.getDashboard);
 router.route("/user/:page").get(AdminController.getAllUser);
+router.route("/teacher-management/").get(AdminController.teacherManagement);
+router.route("/student-management/").get(AdminController.studentManagement);
+router.route("/editUser/:id").get(AdminController.editUser);
+router.route("/editUser/:id").post(AdminController.editedUser);
+router.route("/change-password/:id").get(AdminController.getUserChangePassword);
+router.route("/change-password/:id").post(AdminController.postUserChangePassword);
+
 module.exports = router;
