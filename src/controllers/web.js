0cb145983bef364d37704bd1735f0c362b58030a
Sang <enpro1955@gmail.com>
2021-01-14 21:51:54 +0700

    [Course] Course Feedback and Content

--- src/controllers/web.js
+++ src/controllers/web.js
@@ -1,5 +1,9 @@
 const courseModel = require("../models/Course");
 const userModel = require("../models/User")
+const chapterModel = require("../models/Chapter")
+const fbModel = require("../models/Feedback")
+const lessonModel = require("../models/Lesson");
+const Feedback = require("../models/Feedback");
 
 const getHomePage = async (req, res, next) => {
   const limTop10 = 10;
@@ -20,18 +24,38 @@ const courseDetail = async (req, res, next) => {
   const course = await query.findOne().lean();
   const limit = 5
   const top5 = await courseModel.find({sub_category: course.sub_category}).lean().limit(limit)
-  const teacher = await userModel.findOne({_id: course.teacher}).lean()
+  const teacher = await userModel.findOne({_id: course.author_id}).lean()
+  let chapters = []
+  let l = []
   var d = new Date(course.updatedAt)
   course.updatedAt = d.toLocaleString()
   for(i = 0; i<5;i++) {
     var temp = new Date(top5[i].updatedAt)
     top5[i].updatedAt =temp.toLocaleString()
   }
+  for (i = 0;i < course.chapters.length; i++){
+    chapters[i] = await chapterModel.findOne({_id: course.chapters[i]}).lean()
+    for(j = 0; j < chapters[i].lessons.length; j++){
+      chapters[i].lessons[j] = await lessonModel.findOne({_id: chapters[i].lessons[j]}).lean()
+    }
+
+  }
+
+  const fb = await fbModel.find({courseID: course._id}).lean()
+  for(i=0;i<fb.length;i++){
+    const t = await userModel.findOne({_id: fb[i].studentID}).lean()
+    fb[i].studentID = t.fullname
+    fb[i].ava = t.avatar
+  }
+  console.log(fb)
+
   res.render("pages/courses/details", {
     course,
     top5,
     title: "Detail",
-    teacher
+    teacher,
+    chapters,
+    fb
   });
 };
 
